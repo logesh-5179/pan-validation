6de94cab01794967102e8b48442f32e410d5ac0a
logesh-5179 <logeshmb5179@gmail.com>
2024-07-30 20:44:53 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,12 @@
-function validate(){
-    var val=document.getElementById(text);
-    
-    if(val==null || val==""){
-        alert("Please enter")
+function validate() {
+    const pan = document.getElementById('text').value;
+    const pattern = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
+
+    if (pattern.test(pan)) {
+        alert("Valid PAN Card");
+        return true;
+    } else {
+        alert("Invalid PAN Card");
         return false;
     }
 }
